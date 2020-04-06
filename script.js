6bac025eea53fd0e64e0b83e7ae8ae026f3b045b
Kazak Dmitry <kazak.dmitry1989@mail.ru>
2020-04-06 19:26:17 +0300

    Feat:add some keybord eng

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function start() {
-  console.log('sdsd');
+const keyBordEng = {
+  0: [['~', '`'], ['!', '1'], ['@', '2'], ['#', '3'], ['$', 4], ['%', '5'], [':', '6'], ['?', '7'], ['*', '8'], ['(', '9'], [')', '0'], ['_', '-'], ['+', '='], ['', 'Backspace']],
+  1: [['', 'Tab'], ['', 'Q'], ['', 'W'], ['', 'E'], ['', 'R'], ['', 'T'], ['', 'Y'], ['', 'U'], ['', 'I'], ['', 'O'], ['', 'P'], ['', '['], ['', ']'], ['/', '\\'], ['', 'DEL']],
+  2: [],
+  3: [],
+  4: [],
+};
+function load() {
+  const table = document.createElement('table');
+  const body = document.getElementsByTagName('body');
+  body[0].append(table);
+  for (let i = 0; i < 5; i += 1) {
+    const tr = document.createElement('tr');
+    table.append(tr);
+    for (let k = 0; k < keyBordEng[i].length; k += 1) {
+      const td = document.createElement('td');
+      const div = document.createElement('div');
+      td.append(div);
+      div.append(keyBordEng[i][k][0]);
+      tr.append(td);
+      td.append(keyBordEng[i][k][1]);
+    }
+  }
 }
-start();
+window.onload = load;
