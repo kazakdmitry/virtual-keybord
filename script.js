08730ae975720567818ab5d682fb3f1c8330bb9e
Kazak Dmitry <kazak.dmitry1989@mail.ru>
2020-04-07 10:55:28 +0300

    Feat:keybord is done!

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const keyBordEng = {
   0: [['~', '`'], ['!', '1'], ['@', '2'], ['#', '3'], ['$', 4], ['%', '5'], [':', '6'], ['?', '7'], ['*', '8'], ['(', '9'], [')', '0'], ['_', '-'], ['+', '='], ['', 'Backspace']],
   1: [['', 'Tab'], ['', 'Q'], ['', 'W'], ['', 'E'], ['', 'R'], ['', 'T'], ['', 'Y'], ['', 'U'], ['', 'I'], ['', 'O'], ['', 'P'], ['', '['], ['', ']'], ['/', '\\'], ['', 'DEL']],
-  2: [],
-  3: [],
-  4: [],
+  2: [['', 'Caps Lock'], ['', 'A'], ['', 'S'], ['', 'D'], ['', 'F'], ['', 'G'], ['', 'H'], ['', 'J'], ['', 'K'], ['', 'L'], ['', ';'], ['', '\''], ['', 'ENTER']],
+  3: [['', 'Shift'], ['', '\\'], ['', 'Z'], ['', 'X'], ['', 'C'], ['', 'V'], ['', 'B'], ['', 'N'], ['', 'M'], ['', '.'], ['', ','], ['', ''], ['', '&uarr;'	], ['', 'Shift']],
+  4: [['', 'Ctrl'], ['', 'Win'], ['', 'Alt'], ['', ''], ['', 'Alt'], ['', 'Ctrl'], ['', '&larr;'], ['', '&darr;'], ['', '&rarr;']],
 };
 function load() {
   const table = document.createElement('table');
@@ -15,10 +15,25 @@ function load() {
     for (let k = 0; k < keyBordEng[i].length; k += 1) {
       const td = document.createElement('td');
       const div = document.createElement('div');
-      td.append(div);
-      div.append(keyBordEng[i][k][0]);
       tr.append(td);
-      td.append(keyBordEng[i][k][1]);
+      if ((i === 0) && (k === keyBordEng[0].length - 1)) {
+        td.colSpan = 3;
+      }
+      if ((i === 2) && (k === keyBordEng[2].length - 1)) {
+        td.colSpan = 2;
+      }
+      if ((i === 2) && (k === 0)) {
+        td.colSpan = 2;
+      }
+      if ((i === 3) && (k === 0)) {
+        td.colSpan = 2;
+      }
+      if ((i === 4) && (k === 3)) {
+        td.colSpan = 7;
+      }
+      td.innerHTML = keyBordEng[i][k][1];
+      td.append(div);
+      div.append(keyBordEng[i][k][0]); 
     }
   }
 }
