04771147c64f3089e8655c561b575eb824a2d053
Kazak Dmitry <kazak.dmitry1989@mail.ru>
2020-04-07 14:01:12 +0300

    Feat:animation keyUp keyDown

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const keyBordEng = {
-  0: [['~', '`'], ['!', '1'], ['@', '2'], ['#', '3'], ['$', 4], ['%', '5'], [':', '6'], ['?', '7'], ['*', '8'], ['(', '9'], [')', '0'], ['_', '-'], ['+', '='], ['', 'Backspace']],
-  1: [['', 'Tab'], ['', 'Q'], ['', 'W'], ['', 'E'], ['', 'R'], ['', 'T'], ['', 'Y'], ['', 'U'], ['', 'I'], ['', 'O'], ['', 'P'], ['', '['], ['', ']'], ['/', '\\'], ['', 'DEL']],
-  2: [['', 'Caps Lock'], ['', 'A'], ['', 'S'], ['', 'D'], ['', 'F'], ['', 'G'], ['', 'H'], ['', 'J'], ['', 'K'], ['', 'L'], ['', ';'], ['', '\''], ['', 'ENTER']],
-  3: [['', 'Shift'], ['', '\\'], ['', 'Z'], ['', 'X'], ['', 'C'], ['', 'V'], ['', 'B'], ['', 'N'], ['', 'M'], ['', '.'], ['', ','], ['', ''], ['', '&uarr;'	], ['', 'Shift']],
-  4: [['', 'Ctrl'], ['', 'Win'], ['', 'Alt'], ['', ''], ['', 'Alt'], ['', 'Ctrl'], ['', '&larr;'], ['', '&darr;'], ['', '&rarr;']],
+  0: [['~', '`', 'Backquote'], ['!', '1', 'Digit1'], ['@', '2', 'Digit2'], ['#', '3', 'Digit3'], ['$', 4, 'Digit4'], ['%', '5', 'Digit5'], [':', '6', 'Digit6'], ['?', '7', 'Digit7'], ['*', '8', 'Digit8'], ['(', '9', 'Digit9'], [')', '0', 'Digit0'], ['_', '-', 'Minus'], ['+', '=', 'Equal'], ['', 'Backspace', 'Backspace']],
+  1: [['', 'Tab', 'Tab'], ['', 'Q', 'KeyQ'], ['', 'W', 'KeyW'], ['', 'E', 'KeyE'], ['', 'R', 'KeyR'], ['', 'T', 'KeyT'], ['', 'Y', 'KeyY'], ['', 'U', 'KeyU'], ['', 'I', 'KeyI'], ['', 'O', 'KeyO'], ['', 'P', 'KeyP'], ['', '[', 'BracketLeft'], ['', ']', 'BracketRight'], ['/', '\\', 'Backslash'], ['', 'DEL', 'Delete']],
+  2: [['', 'Caps Lock', 'CapsLock'], ['', 'A', 'KeyA'], ['', 'S', 'KeyS'], ['', 'D', 'KeyD'], ['', 'F', 'KeyF'], ['', 'G', 'KeyG'], ['', 'H', 'KeyH'], ['', 'J', 'KeyJ'], ['', 'K', 'KeyK'], ['', 'L', 'KeyL'], ['', ';', 'Semicolon'], ['', '\'', 'Quote'], ['', 'ENTER', 'Enter']],
+  3: [['', 'Shift', 'ShiftLeft'], ['', '\\', 'Backslash'], ['', 'Z', 'KeyZ'], ['', 'X', 'KeyX'], ['', 'C', 'KeyC'], ['', 'V', 'KeyV'], ['', 'B', 'KeyB'], ['', 'N', 'KeyN'], ['', 'M', 'KeyM'], ['', '.', 'Period'], ['', ',','Comma'], ['', '/', 'Slash'], ['', '&uarr;', 'ArrowUp'], ['', 'Shift', 'ShiftRight']],
+  4: [['', 'Ctrl', 'ControlLeft'], ['', 'Win', 'MetaLeft'], ['', 'Alt', 'AltLeft'], ['', '', 'Space'], ['', 'Alt', 'AltRight'], ['', 'Ctrl', 'ControlRight'], ['', '&larr;', 'ArrowLeft'], ['', '&darr;', 'ArrowDown'], ['', '&rarr;', 'ArrowRight']],
 };
 function load() {
+  const textArea = document.createElement('textarea');
   const table = document.createElement('table');
   const body = document.getElementsByTagName('body');
+  body[0].append(textArea);
+  textArea.onkeydown = keyDownFunc;
+  textArea.onkeyup = keyUpFunc;
   body[0].append(table);
   for (let i = 0; i < 5; i += 1) {
     const tr = document.createElement('tr');
@@ -33,8 +37,27 @@ function load() {
       }
       td.innerHTML = keyBordEng[i][k][1];
       td.append(div);
-      div.append(keyBordEng[i][k][0]); 
+      div.append(keyBordEng[i][k][0]);
     }
   }
 }
 window.onload = load;
+
+function keyDownFunc(event) {
+  console.log(event.code);
+  searchKeyCode(event.code, 'yellow', '5px 5px 5px 5px blue');
+}
+function keyUpFunc(event) {
+  searchKeyCode(event.code, 'black', 'none');
+}
+function searchKeyCode(code, color, shadow) {
+  const trTags = document.querySelectorAll('tr');
+  for (let i = 0; i < 5; i += 1) {
+    for (let k = 0; k < keyBordEng[i].length; k += 1) {
+      if (code === keyBordEng[i][k][2]) {
+        trTags[i].children[k].style.backgroundColor = color;
+        trTags[i].children[k].style.boxShadow = shadow;
+      }
+    }
+  }
+}
